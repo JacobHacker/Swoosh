07e65bdbfc0b77910f38b93df179ac1c10cfb83f
Jacob Hacker <jacob@tekkna.org>
2013-03-08 14:37:43 -0600

    Add example files + README

---
+++ config.ex.php
@@ -0,0 +1,12 @@
+<?php
+
+$sitename = "Swoosh";
+
+// This must be an existing sqlite 3 db
+$dbfile = "sqlite.sql3";
+// Hostname, http://example.com
+$hostname = "http://localhost";
+// If we're in a folder other than web root
+$webroot = "/testsite";
+// Removes link, and blocks access to register.php
+$allow_registration = false;
